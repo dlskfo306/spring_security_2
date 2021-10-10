b5ca4796b806fb1b3b7c1de4008692a8fc23bb78
dlskfo306 <dlskfo306@gmail.com>
2021-10-10 16:07:18 +0900

    211010 - CSRF

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -30,7 +30,7 @@ public class HomeController {
 	@GetMapping("login/loginForm")
 	public String loginForm() {
 	    log.info("Welcome Login Form");
-	    return "login/loginForm2";
+	    return "login/loginForm";
 	}
 	
 	//DB custom
